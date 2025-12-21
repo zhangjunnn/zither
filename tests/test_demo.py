60138b350aefc1843ad965d080427e0b3063b340
zhangjunnn <jun@gradual.com>
2025-12-21 12:07:02 +0800

    modify the test case

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -2,22 +2,22 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-classTestBaidu:
+class TestBaidu:
     """百度搜索测试用例"""
 
-    defsetup_method(self):
+    def setup_method(self):
         """每个测试方法前执行"""
         # 创建浏览器驱动
         self.driver = webdriver.Chrome()  # 如果驱动在PATH中，可以直接这样写
         self.driver.implicitly_wait(10)   # 隐式等待10秒
         self.driver.maximize_window()     # 最大化窗口
 
-    defteardown_method(self):
+    def teardown_method(self):
         """每个测试方法后执行"""
         # 关闭浏览器
         self.driver.quit()
 
-    deftest_baidu_search(self):
+    def test_baidu_search(self):
         """测试百度搜索功能"""
         # 打开百度
         self.driver.get("https://www.baidu.com")
